4d199f10d99f648250878c08f3fa7d2f2e831112
soeur <juzi201314@gmail.com>
2022-01-17 12:50:42 +0800

    添加一个子命令rcgen, 用于快速生成自签名证书

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,52 @@ use std::path::PathBuf;
 #[tokio::main]
 async fn main() {
     let args = argh::from_env::<Arguments>();
-    couscous::run(args.conf).await.unwrap();
+
+    match args.sub {
+        SubCommand::Run(args) => {
+            couscous::run(args.conf).await.unwrap();
+        }
+        SubCommand::RcGen(args) => {
+            let out = args.out_dir.unwrap_or_else(|| PathBuf::from("."));
+            let cert = rcgen::generate_simple_self_signed(args.hosts).unwrap();
+            std::fs::write(out.join("cert.pem"), cert.serialize_pem().unwrap()).unwrap();
+            std::fs::write(out.join("key.pem"), cert.serialize_private_key_pem()).unwrap();
+        }
+    }
 }
 
 /// arguments
 #[derive(argh::FromArgs)]
 struct Arguments {
+    #[argh(subcommand)]
+    sub: SubCommand,
+}
+
+#[derive(argh::FromArgs)]
+#[argh(subcommand)]
+enum SubCommand {
+    Run(Run),
+    RcGen(RcGen),
+}
+
+#[derive(argh::FromArgs)]
+#[argh(subcommand, name = "run")]
+/// run couscous
+struct Run {
     /// configuration file
     #[argh(option, short = 'c')]
     conf: PathBuf,
+}
+
+#[derive(argh::FromArgs)]
+#[argh(subcommand, name = "rcgen")]
+/// gen self signed certificate
+struct RcGen {
+    #[argh(positional)]
+    /// hosts
+    hosts: Vec<String>,
+
+    #[argh(option, short = 'o')]
+    /// out dir
+    out_dir: Option<PathBuf>,
 }
